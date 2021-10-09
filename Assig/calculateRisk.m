b143eeaea6e125ac207277ba7f45fe82c53d7655
mugdharane31 <71972547+mugdharane31@users.noreply.github.com>
2021-10-09 16:37:12 -0400

    Delete calculateRisk.m

--- Assig/calculateRisk.m
+++
@@ -1,6 +0,0 @@
-function risk_mean = calculateRisk(x, y, theta)
-    funct = returnFunction(x, theta);
-    risk = (y-1).*log(1 - funct) - y.*log(funct);
-    risk(isnan(risk )) = 0;
-    risk_mean = mean(risk);
-end
