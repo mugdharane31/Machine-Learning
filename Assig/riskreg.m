2bceeae4d5135368ee2b6afc8ab282aad39181d8
mugdharane31 <71972547+mugdharane31@users.noreply.github.com>
2021-10-09 16:37:03 -0400

    Delete riskreg.m

--- Assig/riskreg.m
+++
@@ -1,9 +0,0 @@
-function [errTrain, model, errTest] = riskreg(xTrain, yTrain, lambda, xTest, yTest)
-    
-    xtx = (xTrain' * xTrain);
-    model = (xTrain' * xTrain + lambda * eye(size(xtx))) \ (xTrain' * yTrain);
-    errTrain = (1/(2*length(xTrain)))*(sum((yTrain - xTrain*model).^2));
-    if(nargin == 5)
-           errTest = (1/(2*length(xTest)))*(sum((yTest - xTest*model).^2));
-    end
-end
